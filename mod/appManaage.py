ab96ec0c3ec31f11ee5cfb455b441431cb5bdd77
hinayand <yang2009120701@outlook.com>
2021-12-07 15:28:09 +0000

    fix some bugs

--- mod/appManaage.py
+++ mod/appManaage.py
@@ -1,1 +1,17 @@
+import configTool
+configWriteObject=configTool.ConfigWrite()
+configReadObject=configTool.ConfigRead()
 
+class AppManaage(object):
+    def __init__(self):
+        pass
+    def appRegister(self,appName,appPath):
+        self.appDatabase=configReadObject.readJSONConfig('config/appLib.conf')
+        self.appDatabase[appName]=appPath
+        configWriteObject.writeByJSON(self.appDatabase,'config/appLib.conf')
+        del self.appDatabase
+    def appUnRegister(self,appName):
+        self.appDatabase=configReadObject.reaeJSONConfig('config/appLib.conf')
+        del self.appDatabase[appName]
+        configWriteObject.writeByJSON(self.appDatabade,'config/appLib.conf')
+        delself.appDatabase
--- mod/pyShell.py
+++ mod/pyShell.py
@@ -1,20 +1,37 @@
 import asyncio
 class PyShell(object):
-  def __init__(self):
-    self.pyshellCommand=0
-    self.pyshellTerminalNum=0 
-  async def shell(self):
-    while True:
-      self.pyshellCommand=input('pyshell$ ')
-      if self.pyshellCommand=='exit':
-       break
-      elif self.pyshellCommand=='run':
-       self.appName=input('Please enter App Name:') 
-       import appLib
-       appLoaderObject=appLib.AppLoader()
-       appLoaderObject.loadAppByName(self.appName)
-      else:
-       exec(self.pyshellCommand)
-  def creatShell(self): 
-    asyncio.run(self.shell())
-    self.pyshellTerminalNum=self.pyshellTerminalNum   
+    def __init__(self):
+        self.pyshellCommand=0
+        self.pyshellTerminalNum=0 
+    async def shell(self):
+        while True:
+            self.pyshellCommand=input('pyshell$ ')
+            if self.pyshellCommand=='exit':
+                break
+            elif self.pyshellCommand=='run':
+                self.appName=input('Please enter App Name:') 
+                import appLib
+                appLoaderObject=appLib.AppLoader()
+                appLoaderObject.loadAppByName(self.appName)
+                del self.appName
+            elif self.pyshellCommand=='appManaage':
+                self.appName=input('Please enter App Name:')
+                self.appManaageOpthion=input('What do you want to do?\nUnRegister\nRegister')
+                if self.appManaageOpthion=='UnRegister':
+                    import appManaage
+                    appUnRegisterObject=appManaage.AppManaage()
+                    appUnRegisterObject.appUnRegister(self.appName)
+                    del appUnRegisterObject
+                else:
+                    self.appPath=input('Please enter App Path:')
+                    import appManaage
+                    appRegisterObject=appManaage.AppManaage()
+                    appRegisterObject.appRegister(self.appName,self.appPath)	
+                    del appRegisterObject
+                del self.appName
+                del self.appManaageOpthion
+            else:
+                exec(self.pyshellCommand)
+    def creatShell(self):
+         asyncio.run(self.shell())
+         self.pyshellTerminalNum=self.pyshellTerminalNum+1
