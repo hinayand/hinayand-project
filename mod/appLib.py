eac07d91579db89d308249e72f038a2e13092256
hinayand <yang2009120701@outlook.com>
2021-12-08 12:21:34 +0000

    fix some bugs

--- mod/appLib.py
+++ mod/appLib.py
@@ -10,14 +10,6 @@ class AppLoader(AppLib):
   def __init__(self):
     pass
   def loadAppByName(self,appName):
-    self.appDatabase=configReadObject.readJSONConfig('config/appLib.conf') 
-    for key in self.appDatabase:
-      if key==appName:
-        self.appIO=open(self.appDatabase[key]) 
-        self.appResult=self.appIO.readlines() 
-        for i in range(len(self.appResult)):
-          exec(self.appResult[i])
-        del self.appIO
-        del self.appResult
-      else:
-        print('Error:load App fail!')
+    import sys
+    sys.path.append('./app/')
+    exec('import {}'.format(appName))
