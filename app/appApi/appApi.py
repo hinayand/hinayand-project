0097cde3db42568d8d41713737f1d280b268baf8
hinayand <yang2009120701@outlook.com>
2021-12-12 08:00:47 +0000

    v0.0.3 fix some bugs and edit "app/appApi/app Api.py" file,Make a Network AppApi

--- app/appApi/appApi.py
+++ app/appApi/appApi.py
@@ -13,3 +13,22 @@ class AppNet(AppApi):
         appOpenObject=appLib.AppLoader()
         appOpenObject.loadAppByName(appName)
         del appOpenObject
+    def showMessage(self,message):
+        print(message)
+    def getUserInput(self,prompt):
+        return input(prompt)
+
+class AppFileIO(AppApi):
+    def __init__(self):
+        pass
+    def writeFile(self,filePath,text,mode):
+        self.appFileWriteIO=open(filePath,mode)
+        self.appFileWeiteIO.write(text)
+        del self.appFileWriteIO
+    def readFile(self,filePath):
+        self.appFileReadIO=open(filePath)
+        return self.appFileReadIO.read()
+        del self.appFileReadIO
+    def readNetworkFile(self,httpFilePath):
+        import requests
+        return requests.get(httpFilePath).text
